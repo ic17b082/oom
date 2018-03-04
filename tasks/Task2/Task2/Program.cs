b58e452cdc36cb6d6545e398264461f55d990e85
Thomas Neugschwandtner <ic17b082@technikum-wien.at>
2018-03-04 16:52:09 +0100

    add project Task2

---
+++ tasks/Task2/Task2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Kundendaten
+{
+    private string Name;
+    private int Kundennummer;
+
+    public Kundendaten(string kunde, int nummer)
+    {
+        if (kunde == null || kunde.Length < 1)
+            throw new Exception("Kein Name");
+        if (nummer < 0)
+            throw new Exception("Keine gueltige Kundennummer");
+        Name = kunde;
+        Kundennummer = nummer;
+    }
+    public string GetName() => Name;
+    public int GetKundennummer() { return Kundennummer; }
+
+    public void SetKundennummer(int neueNummer)
+    {
+        if (neueNummer < 0)
+            throw new Exception("Negative Nummer");
+        Kundennummer = neueNummer;
+    }
+}
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Kundendaten eins = new Kundendaten("Mustermann Max", 1);
+                Console.WriteLine($"Kunde: {eins.GetKundennummer()} { eins.GetName()} ");
+                eins.SetKundennummer(5);
+                Console.WriteLine($"Kunde: {eins.GetKundennummer()} { eins.GetName()} ");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Fehler ({e.Message})");
+            }
+        }
+    }
+}
