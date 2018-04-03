d1b2295434ea4840eccaa03d392f06272eac618c
Thomas Neugschwandtner <ic17b082@technikum-wien.at>
2018-04-03 14:56:20 +0200

    add project Task4

---
+++ tasks/Task4/Task4/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Globalization;
+using Newtonsoft.Json;
+
+namespace Task4
+{
+    interface IPerson
+    {
+        void Print();
+    }
+
+    class Kundendaten : IPerson
+    {
+        public string KName;
+        public string KAdresse;
+        public int Kundennummer;
+
+        public Kundendaten(string kunde, string kundenadresse, int nummer)
+        {
+            if (string.IsNullOrWhiteSpace(kunde))
+                throw new ArgumentException("Kein Name", nameof(kunde));
+            if (string.IsNullOrWhiteSpace(kundenadresse))
+                throw new ArgumentException("Kein Name", nameof(kundenadresse));
+            if (nummer < 0)
+                throw new ArgumentException("Keine gueltige Kundennummer", nameof(nummer));
+
+            KName = kunde;
+            KAdresse = kundenadresse;
+            neueNummer(nummer);
+        }
+
+        public string GetKName => KName;
+        public string GetKAdresse => KAdresse;
+        public int GetKundennummer => Kundennummer;
+
+        public void neueNummer(int nummer)
+        {
+            if (nummer < 0)
+                throw new ArgumentException("Negative Nummer", nameof(nummer));
+            Kundennummer = nummer;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"\nKundendaten: {GetKName} \nAdresse: {GetKAdresse} \nKundennummer: {GetKundennummer}");
+        }
+    }
+
+    class Mitarbeiter : IPerson
+    {
+       
+        public string MName;
+        public string MAdresse;
+        public string Taetigkeit;
+        public int Dienstnummer;
+
+        public Mitarbeiter(string arbeiter, string madresse, string anstellung, int id)
+        {
+            if (string.IsNullOrWhiteSpace(arbeiter))
+                throw new ArgumentException("Kein Name", nameof(arbeiter));
+            if (string.IsNullOrWhiteSpace(madresse))
+                throw new ArgumentException("Keine Adresse", nameof(madresse));
+            if (string.IsNullOrWhiteSpace(anstellung))
+                throw new ArgumentException("Keine gueltige Anstellung", nameof(anstellung));
+            if (id < 0)
+                throw new ArgumentException("Keine gueltige Bedienstetennummer", nameof(id));
+
+            MName = arbeiter;
+            MAdresse = madresse;
+            Taetigkeit = anstellung;
+            Dienstnummer = id;
+        }
+        public string GetMName => MName;
+        public string GetMAdresse => MAdresse;
+        public string GetTaetigkeit => Taetigkeit;
+        public int GetDienstnummer => Dienstnummer;
+
+        public void Print()
+        {
+            Console.WriteLine($"\nMitarbeiter: {GetMName} \nAdresse: {GetMAdresse} \nTaetigkeit: {GetTaetigkeit} \nDienstnumer: {GetDienstnummer}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Kundendaten ersterk = new Kundendaten("Max Mustermann", "Musterweg 15", 1);
+                ersterk.Print();
+                ersterk.neueNummer(2);
+                ersterk.Print();
+                Kundendaten zweiterk = new Kundendaten("Ludwig Leiner", "Kikagasse 68", 2);
+                Mitarbeiter ersterm = new Mitarbeiter("Karl Klumpat", "Forstgasse 27", "Elektriker", 1);
+                Mitarbeiter zweiterm = new Mitarbeiter("Toni Toner", "Druckstrasse 87", "Assistent", 2);
+
+                IPerson[] PersonArray = { ersterk,zweiterk, ersterm, zweiterm, new Kundendaten("Ilse Ilsensberger", "Ilsenweg 30", 3), new Mitarbeiter("Koalrina Karlson", "Karlsonweg 12", "EDV-Technikerin", 3) };
+
+                foreach (IPerson Person in PersonArray)
+                {
+                    Person.Print();
+                }
+
+                var jsonsettings = new JsonSerializerSettings() { Formatting = Formatting.Indented, TypeNameHandling = TypeNameHandling.Auto };
+                var json = JsonConvert.SerializeObject(PersonArray, jsonsettings);
+                Console.WriteLine(json);
+
+                var itemsfromjson = JsonConvert.DeserializeObject<IPerson[]>(json, jsonsettings);
+                foreach (var Actuator in itemsfromjson)
+                {
+                    Actuator.Print();
+                }
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine($"Error ({error.Message})");
+            }
+        }
+    }
+}
---
+++ tasks/Task4/Task4/Tests.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using NUnit.Framework;
+using Task4;
+
+namespace Task4
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void CanCreateKundendaten()
+        {
+            var x = new Kundendaten("Max Mustermann", "Musterweg 15", 1);
+            Assert.IsTrue(x.KName == "Max Mustermann");
+            Assert.IsTrue(x.KAdresse == "Musterweg 15");
+            Assert.IsTrue(x.Kundennummer == 1);
+        }
+
+        [Test]
+        public void CanCreateMitarbeiter()
+        {
+            var x = new Mitarbeiter("Karl Klumpat", "Forstgasse 27", "Elektriker", 1);
+            Assert.IsTrue(x.MName == "Karl Klumpat");
+            Assert.IsTrue(x.MAdresse == "Forstgasse 27");
+            Assert.IsTrue(x.Taetigkeit == "Elektriker");
+            Assert.IsTrue(x.Dienstnummer == 1);
+        }
+
+        [Test]
+        public void CannotCreateNegativeNumber()
+        {
+            Assert.Catch(() =>
+            {
+                var x = new Kundendaten("Max Mustermann", "Musterweg 15", -1);
+            });
+        }
+
+        [Test]
+        public void CannotCreateWithoutName1()
+        {
+            Assert.Catch(() =>
+            {
+                var x = new Mitarbeiter(null, "Forstgasse 27", "Elektriker", 1);
+            });
+        }
+
+        [Test]
+        public void CannotCreateWithoutName2()
+        {
+            Assert.Catch(() =>
+            {
+                var x = new Mitarbeiter("", "Forstgasse 27", "Elektriker", 1);
+            });
+        }
+
+        [Test]
+        public void CanChangeKundennummer()
+        {
+            var x = new Kundendaten("Max Mustermann", "Musterweg 15", 1);
+            Assert.IsTrue(x.Kundennummer == 1);
+            x.neueNummer(66);
+            Assert.IsTrue(x.Kundennummer == 66);
+        }
+
+        [Test]
+        public void CannotChangeNegativeNumber()
+        {
+            Assert.Catch(() =>
+            {
+                var x = new Kundendaten("Max Mustermann", "Musterweg 15", 1);
+                x.neueNummer(-66);
+            });
+        }
+
+        [Test]
+        public void CannotCreateWithoutAddress1()
+        {
+            Assert.Catch(() =>
+            {
+                var x = new Mitarbeiter("Karl Klumpat", null, "Elektriker", 1);
+            });
+        }
+
+        [Test]
+        public void CannotCreateWithoutAddress2()
+        {
+            Assert.Catch(() =>
+            {
+                var x = new Mitarbeiter("Karl Klumpat", "", "Elektriker", 1);
+            });
+        }
+    }
+}
